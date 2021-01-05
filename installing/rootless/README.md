3ea4c1219c04a2c20e2133b1be1860ba2b8ead39
Wes Higbee <wes.mcclure@gmail.com>
2021-01-05 15:56:41 -0500

    rootless

---
+++ installing/rootless/README.md
@@ -0,0 +1,6 @@
+# rootless build
+
+TLDR: VM with the rootless build of docker.
+
+- <https://docs.docker.com/engine/security/rootless/>
+- [wes-docs - rootless](https://github.com/g0t4/wes-docs/blob/master/tools/dockers/install/rootless.md)
---
+++ installing/rootless/config.learn.ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Tools I use in ubuntu learning environments
+#
+# wes-docs / searching ubuntu packages
+# - https://github.com/g0t4/wes-docs/blob/master/linux/ubuntu/searching.ubuntu.packages.md
+# 
+
+sudo apt-get update
+
+sudo apt-get install -y \
+  tree \
+  psmisc \
+  pslist
+  # todo batcat
