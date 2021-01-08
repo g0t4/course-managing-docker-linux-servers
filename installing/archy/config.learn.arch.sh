b5f0670eb76d98af9a7460988903e5c74ae158fb
Wes Higbee <wes.mcclure@gmail.com>
2021-01-08 02:10:12 -0500

    add a few tools I like to have on my VMs

---
+++ installing/archy/config.learn.arch.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+sudo pacman --noconfirm -Sy \
+  tree \
+  jq
+
+
+
+
+## NOTES
+
+# pstree installed is great!
+
+# pacman -Sh # help for options to -S (remote packages)
+# find packages
+# pacman -Ss tree # search remote repositories for string, "tree" in this case
+
+
+## local packages
+# what package owns a file
+# pacman -Qo pstree
+# /usr/bin/pstree is owned by psmisc 23.3-4
