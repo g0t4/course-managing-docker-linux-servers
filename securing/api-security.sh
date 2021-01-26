8a80830552f201a95e12e6bb7f92d5d070d1c9c9
Wes Higbee <wes.mcclure@gmail.com>
2021-01-25 19:31:10 -0500

    new security samples from remote access

--- securing/api-security.sh
+++ securing/api-security.sh
@@ -4,17 +4,25 @@
 # based on samples: 
   # https://docs.docker.com/engine/api/sdk/#sdk-and-api-quickstart
   # https://docs.docker.com/engine/api/sdk/examples/
+# brew install jq # macOS
+# apt install jq # ubuntu
 
-curl --unix-socket /var/run/docker.sock \
+curl --silent \
+  -X POST http://192.168.30.101:2375/v1.41/images/create?fromImage=alpine&tag=latest
+
+# launch containers!
+curl --silent \
   -H "Content-Type: application/json" \
   -d '{"Image": "alpine", "Cmd": ["echo", "hello world"]}' \
-  -X POST http://localhost/v1.41/containers/create?name=foo
+  -X POST http://192.168.30.101:2375/v1.41/containers/create?name=foo
 
 # query list of running containers
-curl --unix-socket /var/run/docker.sock \
-  -X GET http://localhost/containers/json \
+curl --silent \
+  -X GET http://192.168.30.101:2375/containers/json \
   | jq ".[].Names"
 
+# exit from getdc and run:
+curl --silent -X GET http://192.168.30.101:2375/images/json | jq
 
 # with curl+unix-socket the hostname "localhost" in  "http://localhost" can be whatever
 
