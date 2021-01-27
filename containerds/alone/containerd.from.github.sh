bad6acf4225063b65abe8d323078e11a0783dc6a
Wes Higbee <wes.mcclure@gmail.com>
2021-01-27 15:15:02 -0500

    install script for runc

--- containerds/alone/containerd.from.github.sh
+++ containerds/alone/containerd.from.github.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# From: https://containerd.io/downloads/
+# https://containerd.io/downloads/
 # _VERSION="1.4.3"
+# https://github.com/containerd/containerd/releases/tag/v1.5.0-beta.0
 _VERSION="1.5.0-beta.0"
-_FILE="containerd-${_VERSION}-linux-amd64.tar.gz"
-wget "https://github.com/containerd/containerd/releases/download/v${_VERSION}/${_FILE}"
-tar xvf "${_FILE}"
+__FILE="containerd-${_VERSION}-linux-amd64.tar.gz"
+wget "https://github.com/containerd/containerd/releases/download/v${_VERSION}/${__FILE}"
+tar xvf "${__FILE}"
+sudo cp bin/* /usr/bin
---
+++ containerds/alone/scripts/runc.from.github.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Install /bin/runc pre-built binary
+
+# latest github release:
+# https://github.com/opencontainers/runc/releases/tag/v1.0.0-rc92
+_VERSION="v1.0.0-rc92"
+
+# download runc binary right into /bin as runc so it is in path
+# thus making it discoverable by containerd
+sudo wget \
+  https://github.com/opencontainers/runc/releases/download/${_VERSION}/runc.amd64 \
+  --output-document /bin/runc
+# static linked to https://github.com/seccomp/libseccomp
+
+
+
+
+
+## Further learning about runc
+#
+# Challenge: use runc alone (strip away containerd):
+# https://github.com/opencontainers/runc#using-runc
+#
+# Read the runtime-spec
+# https://github.com/opencontainers/runtime-spec
+# 
+# What are kata-containers?
+# Can you make them work with containerd?
+# https://github.com/kata-containers/kata-containers/releases/tag/2.0.1
