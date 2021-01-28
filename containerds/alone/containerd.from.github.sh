6c6bd7919a97b67e8c8f65b8e378fa2b0551effb
Wes Higbee <wes.mcclure@gmail.com>
2021-01-28 06:56:20 -0500

    comments

--- containerds/alone/containerd.from.github.sh
+++ containerds/alone/containerd.from.github.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # https://containerd.io/downloads/
+# WARNING: check for new containerd version and/or use stable build:
 # _VERSION="1.4.3"
+# 
+# latest version is in beta:
 # https://github.com/containerd/containerd/releases/tag/v1.5.0-beta.0
 _VERSION="1.5.0-beta.0"
 __FILE="containerd-${_VERSION}-linux-amd64.tar.gz"
 wget "https://github.com/containerd/containerd/releases/download/v${_VERSION}/${__FILE}"
 tar xvf "${__FILE}"
+# put binaries (contained|ctr|shims) into path:
 sudo cp bin/* /usr/bin
