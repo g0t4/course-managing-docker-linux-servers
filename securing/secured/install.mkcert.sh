09efe77d05223faa1cb6c550304d26f2f6c3804d
Wes Higbee <wes.mcclure@gmail.com>
2021-02-03 09:22:02 -0500

    add install mkcert

--- securing/secured/install.mkcert.sh
+++ securing/secured/install.mkcert.sh
@@ -4,11 +4,11 @@
 # - https://github.com/FiloSottile/mkcert/releases
 
 _VERSION='1.4.3'
-sudo wget -quiet \
+sudo wget --quiet \
   -O mkcert \
-  https://github.com/FiloSottile/mkcert/releases/download/v${VERSION}/mkcert-v${VERSION}-linux-amd64 
-sudo +x mkcert
-sudo cp mkcert /usr/bin/.
+  "https://github.com/FiloSottile/mkcert/releases/download/v${_VERSION}/mkcert-v${_VERSION}-linux-amd64" 
+sudo chmod +x mkcert
+sudo mv mkcert /usr/bin/.
 
 
 # - consider using it for more than just docker, see its docs for more
